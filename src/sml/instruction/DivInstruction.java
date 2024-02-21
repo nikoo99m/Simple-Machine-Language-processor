61e903df3e354c2efbe0067d04838ebca417e6dd
nikoo99m <nmogha02@student.bbk.ac.uk>
2024-02-21 23:53:54 +0000

    feat: Adding required behaviours for DivInstruction class.

--- src/sml/instruction/DivInstruction.java
+++ src/sml/instruction/DivInstruction.java
@@ -1,4 +1,46 @@
 package sml.instruction;
 
-public class DivInstruction {
+import sml.*;
+
+public class DivInstruction extends Instruction {
+    private final InstructionDestination destination;
+    public static final String OP_CODE = "div";
+
+    public DivInstruction(String label, InstructionDestination destination) {
+        super(label, OP_CODE);
+        this.destination = destination;
+    }
+
+    @Override
+    public int getSize() {
+        return 1 + destination.getSize();
+    }
+
+    @Override
+    public int execute(Machine m) {
+        int y = destination.getValue();
+        Registers registers = m.getRegisters();
+        int lower = registers.get(Registers.RegisterNameImpl.AX);
+        int upper = registers.get(Registers.RegisterNameImpl.DX);
+        long combined = ((long) upper << 32) | (lower & 0xFFFFFFFFL);
+        registers.set(Registers.RegisterNameImpl.AX, (int) (combined / y));
+        registers.set(Registers.RegisterNameImpl.DX, (int) (combined % y));
+
+        return getSize();
+    }
+
+    @Override
+    public String toString() {
+            return getLabelString()  + getOpcode() + " " + destination ;
+        }
+
+    @Override
+    public boolean equals(Object o) {
+        return false;
+    }
+
+    @Override
+    public int hashCode() {
+        return 0;
+    }
 }
