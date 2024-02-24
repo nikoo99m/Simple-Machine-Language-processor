c5fc2497b3294f3aabca92717442de3601b193b7
nikoo99m <nmogha02@student.bbk.ac.uk>
2024-02-24 18:59:30 +0000

    feat: Adding required behaviours for JgeInstruction class.

--- src/sml/instruction/JgeInstruction.java
+++ src/sml/instruction/JgeInstruction.java
@@ -1,4 +1,45 @@
 package sml.instruction;
 
-public class JgeInstruction {
+import sml.Flags;
+import sml.Instruction;
+import sml.Labels;
+import sml.Machine;
+
+public class JgeInstruction extends Instruction {
+    private final Flags FLAG;
+    public static final String OP_CODE = "jge";
+
+
+    public JgeInstruction(String label, String opcode, Flags flag) {
+        super(label, opcode);
+        this.FLAG = flag;
+    }
+
+    @Override
+    public int getSize() {
+        return 2;
+    }
+
+    @Override
+    public int execute(Machine m) {
+        if (!FLAG.getSF() || FLAG.getZF()){
+         return m.getOffset(label);
+    }
+        return getSize();
+    }
+
+    @Override
+    public String toString() {
+        return getOpcode() + " " + getLabelString();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        return false;
+    }
+
+    @Override
+    public int hashCode() {
+        return 0;
+    }
 }
