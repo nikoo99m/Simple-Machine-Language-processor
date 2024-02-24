c96948fb48f027167169871ebe7e2285e4824fa6
nikoo99m <nmogha02@student.bbk.ac.uk>
2024-02-24 19:25:55 +0000

    feat: Adding required behaviours for JneInstruction class.

--- src/sml/instruction/JneInstruction.java
+++ src/sml/instruction/JneInstruction.java
@@ -1,4 +1,42 @@
 package sml.instruction;
 
-public class JneInstruction {
+import sml.Flags;
+import sml.Instruction;
+import sml.Machine;
+
+public class JneInstruction extends Instruction {
+    private final Flags FLAG;
+    public static final String OP_CODE = "jne";
+
+    public JneInstruction(String label, String opcode, Flags flag) {
+        super(label, opcode);
+        FLAG = flag;
+    }
+
+    @Override
+    public int getSize() {
+        return 2;
+    }
+
+    @Override
+    public int execute(Machine m) {
+        if (!FLAG.getZF())
+            return m.getOffset(label);
+        return getSize();
+    }
+
+    @Override
+    public String toString() {
+        return getOpcode();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        return false;
+    }
+
+    @Override
+    public int hashCode() {
+        return 0;
+    }
 }
