a7874d65472145ce27dcb4ae1cd64aaef3386769
nikoo99m <nmogha02@student.bbk.ac.uk>
2024-03-02 22:04:00 +0000

    feat: Implementing equals method in SubInstruction.

--- src/sml/instruction/SubInstruction.java
+++ src/sml/instruction/SubInstruction.java
@@ -7,45 +7,50 @@ import sml.InstructionSource;
 import sml.Machine;
 
 public class SubInstruction extends Instruction {
-    private final InstructionDestination destination;
+    private final InstructionDestination result;
     private final InstructionSource source;
     public static final String OP_CODE = "sub";
 
-    public SubInstruction(String label, InstructionDestination destination, InstructionSource source) {
+    public SubInstruction(String label, InstructionDestination result, InstructionSource source) {
         super(label, OP_CODE);
-        this.destination = destination;
+        this.result = result;
         this.source = source;
     }
 
     @Override
     public int execute(Machine m) {
-        int x = destination.getValue();
+        int x = result.getValue();
         int y = source.getValue();
-        destination.setValue(x - y);
+        result.setValue(x - y);
         return getSize();
     }
 
     @Override
     public int getSize() {
-        return 1 + destination.getSize() + source.getSize();
+        return 1 + result.getSize() + source.getSize();
     }
 
     @Override
     public String toString() {
-        return getLabelString() + getOpcode() + " " + destination + ", " + source;
+        return getLabelString() + getOpcode() + " " + result + ", " + source;
     }
 
     @Override
     public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        SubInstruction that = (SubInstruction) o;
-        return destination.equals(that.destination) && source.equals(that.source);
+        if (this == o) {
+            return true;
+        }
+        if (o instanceof SubInstruction other) {
+            return this.source.equals(other.source)
+                    && this.result.equals(other.result)
+                    && super.equals(other);
+        }
+        return false;
     }
 
     @Override
     public int hashCode() {
-        int result = destination.hashCode();
+        int result = this.result.hashCode();
         result = 31 * result + source.hashCode();
         return result;
     }
