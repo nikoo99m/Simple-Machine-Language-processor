4fd1167836a349b6df45432abc6aa4d443a96e05
nikoo99m <nmogha02@student.bbk.ac.uk>
2024-03-02 22:03:35 +0000

    feat: Implementing equals method in MovInstruction.

--- src/sml/instruction/MovInstruction.java
+++ src/sml/instruction/MovInstruction.java
@@ -8,7 +8,6 @@ import sml.Machine;
 public class MovInstruction extends Instruction {
     private final InstructionDestination result;
     private final InstructionSource source;
-
     public static final String OP_CODE = "mov";
 
     public MovInstruction(String label, InstructionDestination result, InstructionSource source) {
@@ -32,5 +31,23 @@ public class MovInstruction extends Instruction {
     @Override
     public String toString() {
         return getLabelString() + getOpcode() + " " + result + ", " + source;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o instanceof MovInstruction other) {
+            return this.source.equals(other.source)
+                    && this.result.equals(other.result)
+                    && super.equals(other);
+        }
+        return false;
+    }
+
+    @Override
+    public int hashCode() {
+        return 0;
     }
 }
