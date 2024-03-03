10569a05e2782da6ceab8548d1395ab2b3fa0b9b
nikoo99m <nmogha02@student.bbk.ac.uk>
2024-03-03 14:52:09 +0000

    test: Adding test for equal and hashcode behaviours in AddInstructionTest.

--- test/sml/instruction/AddInstructionTest.java
+++ test/sml/instruction/AddInstructionTest.java
@@ -4,7 +4,12 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import sml.*;
+
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static sml.Registers.RegisterNameImpl.*;
@@ -54,4 +59,62 @@ class AddInstructionTest {
         instruction.execute(machine);
         Assertions.assertEquals(106, machine.getRegisters().get(DX));
     }
+
+    static Stream<Arguments> provideTestDataForNotEquals() {
+        Machine machine = new Machine(10);
+        Registers registers = machine.getRegisters();
+        return Stream.of(
+                Arguments.of(new AddInstruction("add1", new OperandRegister(BX, registers), new OperandRegister(AX, registers)),
+                        new AddInstruction("add2", new OperandRegister(BX, registers), new OperandRegister(AX, registers))),
+
+                Arguments.of((new AddInstruction("add1", new OperandRegister(AX, registers), new OperandRegister(AX, registers))),
+                        new AddInstruction("add1", new OperandRegister(BX, registers), new OperandRegister(AX, registers))),
+
+                Arguments.of((new AddInstruction("add1", new OperandRegister(BX, registers), new OperandRegister(AX, registers))),
+                        new AddInstruction("add1", new OperandRegister(BX, registers), new OperandRegister(BX, registers))),
+
+                Arguments.of((new AddInstruction("add1", new OperandRegister(BX, registers), new OperandImmediate(10))),
+                        new AddInstruction("add1", new OperandRegister(BX, registers), new OperandImmediate(2))),
+
+                Arguments.of((new AddInstruction("add1", new OperandMemoryWithBase(2, machine.getMemory(), BX, registers), new OperandRegister(AX, registers))),
+                        new AddInstruction("add1",new OperandMemoryWithBase(1, machine.getMemory(), BX, registers), new OperandRegister(AX, registers))),
+
+                Arguments.of((new AddInstruction("add1", new OperandMemoryWithBase(1, machine.getMemory(), BX, registers), new OperandRegister(AX, registers))),
+                        new AddInstruction("add1",new OperandMemoryWithBase(1, machine.getMemory(), AX, registers), new OperandRegister(AX, registers)))
+        );
+    }
+    @ParameterizedTest
+    @MethodSource("provideTestDataForNotEquals")
+    public void testNotEquals(Instruction obj1, Instruction obj2) {
+        assertNotEquals(obj1, obj2);
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideTestDataForNotEquals")
+    public void testHashCodeNotEquals(Instruction obj1, Instruction obj2) {
+        assertNotEquals(obj1.hashCode(), obj2.hashCode());
+    }
+
+    static Stream<Arguments> provideTestDataForEquals() {
+        Machine machine = new Machine(10);
+        Registers registers = machine.getRegisters();
+        return Stream.of(
+                Arguments.of(new AddInstruction("add1", new OperandRegister(BX, registers), new OperandRegister(AX, registers)),
+                        new AddInstruction("add1", new OperandRegister(BX, registers), new OperandRegister(AX, registers))),
+
+                Arguments.of((new AddInstruction("add1", new OperandRegister(BX, registers), new OperandImmediate(10))),
+                        new AddInstruction("add1", new OperandRegister(BX, registers), new OperandImmediate(10))),
+
+                Arguments.of((new AddInstruction("add1", new OperandMemoryWithBase(1, machine.getMemory(), BX, registers), new OperandRegister(AX, registers))),
+                        new AddInstruction("add1",new OperandMemoryWithBase(1, machine.getMemory(), BX, registers), new OperandRegister(AX, registers)))
+        );
+    }
+    @ParameterizedTest
+    @MethodSource("provideTestDataForEquals")
+    public void testEquals(Instruction obj1, Instruction obj2) {
+
+        assertEquals(obj1, obj2);
+        assertEquals(obj1.hashCode(), obj2.hashCode());
+    }
+
 }
