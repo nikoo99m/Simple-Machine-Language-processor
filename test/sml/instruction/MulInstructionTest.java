7eb4b52a4dc4e5e51602966949712d12fff22c24
nikoo99m <nmogha02@student.bbk.ac.uk>
2024-03-03 14:53:29 +0000

    test: Adding test for equal and hashcode behaviours in MulInstructionTest.

--- test/sml/instruction/MulInstructionTest.java
+++ test/sml/instruction/MulInstructionTest.java
@@ -4,7 +4,12 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import sml.*;
+
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static sml.Registers.RegisterNameImpl.*;
@@ -35,6 +40,7 @@ class MulInstructionTest {
         Instruction instruction = new MulInstruction(null, new OperandRegister(BX, registers));
         instruction.execute(machine);
         Assertions.assertEquals(30, machine.getRegisters().get(AX));
+        Assertions.assertEquals(0, machine.getRegisters().get(DX));
     }
 
     @Test
@@ -44,5 +50,52 @@ class MulInstructionTest {
         Instruction instruction = new MulInstruction(null, new OperandMemory(0, machine.getMemory()));
         instruction.execute(machine);
         Assertions.assertEquals(45, machine.getRegisters().get(AX));
+    }
+
+    static Stream<Arguments> provideTestDataForNotEquals() {
+        Machine machine = new Machine(10);
+        Registers registers = machine.getRegisters();
+        return Stream.of(
+                Arguments.of(new MulInstruction("mul1", new OperandRegister(AX, registers)),
+                        new MulInstruction("mul2", new OperandRegister(AX, registers))),
+
+                Arguments.of((new MulInstruction("mul1", new OperandRegister(AX, registers))),
+                        new MulInstruction("mul1", new OperandRegister(BX, registers))),
+
+                Arguments.of((new MulInstruction("mul1", new OperandMemory(0 ,machine.getMemory()))),
+                        new MulInstruction("mul1", new OperandMemory(3, machine.getMemory()))));
+
+
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideTestDataForNotEquals")
+    public void testNotEquals(Instruction obj1, Instruction obj2) {
+        assertNotEquals(obj1, obj2);
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideTestDataForNotEquals")
+    public void testHashCodeNotEquals(Instruction obj1, Instruction obj2) {
+        assertNotEquals(obj1.hashCode(), obj2.hashCode());
+    }
+
+    static Stream<Arguments> provideTestDataForEquals() {
+        Machine machine = new Machine(10);
+        Registers registers = machine.getRegisters();
+        return Stream.of(
+                Arguments.of(new MulInstruction("mul1", new OperandRegister(AX, registers)),
+                        new MulInstruction("mul1", new OperandRegister(AX, registers))),
+
+                Arguments.of((new MulInstruction("mul1", new OperandMemory(0 ,machine.getMemory()))),
+                        new MulInstruction("mul1", new OperandMemory(0, machine.getMemory()))));
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideTestDataForEquals")
+    public void testEquals(Instruction obj1, Instruction obj2) {
+
+        assertEquals(obj1, obj2);
+        assertEquals(obj1.hashCode(), obj2.hashCode());
     }
 }
