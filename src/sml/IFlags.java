36b6c76b211c775e4004a682d9681c981102cef5
nikoo99m <nmogha02@student.bbk.ac.uk>
2024-03-05 04:19:17 +0000

    docs: Adding JavaDoc to explain the reason of created interfaces.

--- src/sml/IFlags.java
+++ src/sml/IFlags.java
@@ -1,5 +1,8 @@
 package sml;
-
+/**
+ * Defines the interface for Flags, facilitating testing using JMock.
+ * specially to test the 'execute' behavior of jump instructions.
+ */
 public interface IFlags {
     boolean getZF();
     boolean getSF();
--- src/sml/IMachine.java
+++ src/sml/IMachine.java
@@ -1,5 +1,8 @@
 package sml;
-
+/**
+ * Defines the interface for Machine, facilitating testing using JMock.
+ * specially to test the 'execute' behavior of jump instructions.
+ */
 public interface IMachine {
     public void execute();
     public IFlags getFlags();
